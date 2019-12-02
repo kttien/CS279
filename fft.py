107b0375215b6f7e565a5f5156e6a8bbfca09219
nimitd <nimitd@stanford.edu>
2019-12-02 02:15:59 -0800

    Implemented 2D IFFT and Fixed Tests

--- fft.py
+++ fft.py
@@ -2,6 +2,7 @@
 from cmath import exp, pi
 import time
 import numpy as np
+import timeit
  
 
 def dft1D_unopt(x):	
@@ -25,7 +26,7 @@ def dft1D_2(x, c1):
 
 def fft1D_unopt(x):
   N = len(x)
-  if N <= 4: return x
+  if N <= 1: return x
   even = fft1D_unopt(x[0::2])
   odd =  fft1D_unopt(x[1::2])
   T= [exp(-2j*pi*k/N)*odd[k] for k in range(N//2)]
@@ -45,8 +46,8 @@ def inverse_fft1D(x):
 		N = len(x)
 		c1 = 2j*pi/N
 		if N <= 4: return dft1D_2(x, c1)
-		even = fft1D_opt(x[0::2])
-		odd =  fft1D_opt(x[1::2])
+		even = inverse_fft_helper(x[0::2])
+		odd =  inverse_fft_helper(x[1::2])
 		T= [exp(2j*pi*k/N)*odd[k] for k in range(N//2)]
 		return [even[k] + T[k] for k in range(N//2)] + [even[k] - T[k] for k in range(N//2)]
 	result = inverse_fft_helper(x)
@@ -57,40 +58,72 @@ def fft2D(x):
 	second_result = np.asarray([fft1D_opt(first_result.T[col]) for col in range(first_result.shape[1])])
 	return(second_result.T)
 
+def ifft2D(x):
+	first_result = np.asarray([inverse_fft1D(x[row]) for row in range(x.shape[0])])
+	second_result = np.asarray([inverse_fft1D(first_result.T[col]) for col in range(first_result.shape[1])])
+	return(second_result.T)
 
-def test(func_1, func_2, dim, inverse = False):
-	if dim == 1:
-		x = np.random.random(1024)
-	else:
-		x = np.random.rand(256, 256)
-	if inverse:
-		x = np.fft.fft(x)
+
+def tests():
+	x = np.random.random(1024)
 
 	start_time_1 = time.time()
-	f_1_output = func_1(x)
+	f_1_output = fft1D_opt(x)
 	f_1_time = time.time() - start_time_1
 
 	start_time_2 = time.time()
-	f_2_output = func_1(x)
+	f_2_output = dft1D_unopt(x)
 	f_2_time = time.time() - start_time_2
-
-	print('{} took {} seconds to run'.format(func_1.__name__, f_1_time))
-	print('{} took {} seconds to run'.format(func_2.__name__, f_2_time))
+	print("1D FFT took {} seconds, while 1D DFT took {} seconds".format(f_1_time, f_2_time))
 	print('Do output of both functions match? {}'.format(np.allclose(f_1_output, f_2_output)))
-	print()
 
+	start_time_1 = time.time()
+	f_1_output = fft1D_opt(x)
+	f_1_time = time.time() - start_time_1
 
-def run_tests():
-	test(dft1D_unopt, fft1D_unopt, 1)					# test our Fast Fourier Transform against 1D DFT
-	test(dft1D_unopt, fft1D_opt, 1)						# test our optimized Fast Fourier Transform against 1D DFT
-	test(fft1D_opt, np.fft.fft, 1)						# test our 1D FFT against the numpy 1D FFT
-	test(inverse_fft1D, np.fft.ifft, 1, inverse = True)	# test our inverse 1D FFT against the numpy 1D IFFT
-	test(fft2D, np.fft.fft2, 2)							# test our 2D FFT against the numpy 2D FFT
+	start_time_2 = time.time()
+	result = np.fft.fft(x)
+	f_2_time = time.time() - start_time_2
+	print("1D FFT took {} seconds, while Numpy 1D FFT took {} seconds".format(f_1_time, f_2_time))
+	print('Do output of both functions match? {}'.format(np.allclose(f_1_output, result)))
+
+	start_time_1 = time.time()
+	f_1_output = inverse_fft1D(result)
+	f_1_time = time.time() - start_time_1
+
+	start_time_2 = time.time()
+	f_2_output = np.fft.ifft(result)
+	f_2_time = time.time() - start_time_2
+	print("1D IFFT took {} seconds, while Numpy 1D IFFT took {} seconds".format(f_1_time, f_2_time))
+	print('Do output of both functions match? {}'.format(np.allclose(f_1_output, f_2_output)))
+
+	x = np.random.rand(256, 256)
+	start_time_1 = time.time()
+	f_1_output = fft2D(x)
+	f_1_time = time.time() - start_time_1
+
+	start_time_2 = time.time()
+	f_2_output = np.fft.fft2(x)
+	f_2_time = time.time() - start_time_2
+	print("2D FFT took {} seconds, while Numpy 2D FFT took {} seconds".format(f_1_time, f_2_time))
+	print('Do output of both functions match? {}'.format(np.allclose(f_1_output, f_2_output)))
+
+	result = np.fft.fft2(x)
+	start_time_1 = time.time()
+	f_1_output = ifft2D(result)
+	f_1_time = time.time() - start_time_1
+
+	start_time_2 = time.time()
+	f_2_output = np.fft.ifft2(result)
+	f_2_time = time.time() - start_time_2
+	print("2D IFFT took {} seconds, while Numpy 2D IFFT took {} seconds".format(f_1_time, f_2_time))
+	print('Do output of both functions match? {}'.format(np.allclose(f_1_output, f_2_output)))
+	print('Do output of both functions match? {}'.format(np.allclose(f_1_output, f_2_output)))
 
 
 def main():
-	run_tests()
-	
+	tests()
+
 
 
 if __name__ == '__main__':
