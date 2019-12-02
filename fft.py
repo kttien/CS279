4b63c3367c88be23d050a1ee5829eb91b1820085
Kevin Tien <kttien@stanford.edu>
2019-12-01 16:48:18 -0800

    Optimize

--- fft.py
+++ fft.py
@@ -5,12 +5,25 @@ import numpy as np
  
 
 def dft1D(x):	
-	x = np.asarray(x, dtype=float)
-	N = x.shape[0]
-	n = np.arange(N)
-	k = n.reshape((N, 1))
-	M = np.exp(-2j * np.pi * k * n / N)
-	return np.dot(M, x)
+	N = len(x)
+	n_range = np.arange(N)
+	a = np.ndarray(shape=(N, N), dtype=complex)
+	# c1 = -2j*pi/N
+	for n in range(N):
+		# c2 = c1 * n
+		for k in range(N):
+			a[k][n] = exp(-2j * pi * k * n / N)
+	return np.dot(a, x)
+
+def dft1D_2(x, c1):	
+	N = len(x)
+	n_range = np.arange(N)
+	a = np.ndarray(shape=(N, N), dtype=complex)
+	for n in range(N):
+		c2 = c1 * n
+		for k in range(N):
+			a[k][n] = exp(c2*k)
+	return np.dot(a, x)
 
 def fft1D_1(x):
   N = len(x)
@@ -22,24 +35,36 @@ def fft1D_1(x):
 
 def fft1D_2(x):
   N = len(x)
-  if N <= 32: return dft1D(x)
+  c1 = -2j*pi/N
+  if N <= 4: return dft1D_2(x, c1)
   even = fft1D_2(x[0::2])
   odd =  fft1D_2(x[1::2])
   T= [exp(-2j*pi*k/N)*odd[k] for k in range(N//2)]
   return [even[k] + T[k] for k in range(N//2)] + [even[k] - T[k] for k in range(N//2)]
 
-x = np.random.random(1024)
-start_time = time.time()
-dft1D(x)
-end_time = time.time() - start_time
-print(end_time)
 
-start_time = time.time()
-fft1D_1(x)
-end_time = time.time() - start_time
-print(end_time)
 
-start_time = time.time()
-fft1D_2(x)
-end_time = time.time() - start_time
-print(end_time)
+def test_times():
+	x = np.random.random(1024)
+
+	start_time = time.time()
+	dft1D(x)
+	end_time = time.time() - start_time
+	print(end_time)
+
+	start_time = time.time()
+	fft1D_1(x)
+	end_time = time.time() - start_time
+	print(end_time)
+
+	start_time = time.time()
+	fft1D_2(x)
+	end_time = time.time() - start_time
+	print(end_time)
+
+	start_time = time.time()
+	np.fft.fft(x)
+	end_time = time.time() - start_time
+	print(end_time)
+
+test_times()
